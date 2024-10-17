b9441f47384299c03ee0f4b4bc62e69f80a236fb
Juan Camilo Narváez <ulvenforst@gmail.com>
2024-10-16 20:25:58 -0500

    initial template for the frontend

--- filmore/vite.config.js
+++ filmore/vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true, // Enable polling
-      interval: 1000,   // Optional: set the polling interval to 1000ms (1 second)
-    },
-  },
+  plugins: [react()]
 })
