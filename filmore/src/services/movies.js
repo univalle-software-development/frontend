3a4cecbd5b5372b3720c0157f02b194ba2a89c9b
Cristian David <cristian.pacheco@correounivalle.edu.co>
2024-11-30 17:00:45 -0500

    Connecting the app with the backend

--- filmore/src/services/movies.js
+++ filmore/src/services/movies.js
@@ -4,17 +4,8 @@ export const searchMovies = async ({ search }) => {
   if (search === '') return null
 
   try {
-    const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`)
-    const json = await response.json()
-
-    const movies = json.Search
-
-    return movies?.map(movie => ({
-      id: movie.imdbID,
-      title: movie.Title,
-      year: movie.Year,
-      image: movie.Poster
-    }))
+      const response = await fetch(`http://my-app-route-cristiandpt-dev.apps.sandbox-m4.g2pi.p1.openshiftapps.com/search?q=${search}`);
+      return response.json();
   } catch (e) {
     throw new Error('Error searching movies')
   }
