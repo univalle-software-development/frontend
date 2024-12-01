22ccc3078ea0ae145a469152d9d05a2e9b54dd07
Cristian David <cristian.pacheco@correounivalle.edu.co>
2024-11-30 21:27:16 -0500

    Point to the deployed backend

--- filmore/src/services/movies.js
+++ filmore/src/services/movies.js
@@ -1,12 +1,16 @@
 const API_KEY = '4287ad07'
 
 export const searchMovies = async ({ search }) => {
-  if (search === '') return null
+	if (search === '') return null
 
-  try {
-      const response = await fetch(`http://my-app-route-cristiandpt-dev.apps.sandbox-m4.g2pi.p1.openshiftapps.com/search?q=${search}`);
-      return response.json();
-  } catch (e) {
-    throw new Error('Error searching movies')
-  }
+	const apiUrl = import.meta.env.VITE_NODE_ENV === 'production'
+		  ? import.meta.env.VITE_API_URL_PROD
+		  : import.meta.env.VITE_API_URL_DEV;
+	
+	try {
+		const response = await fetch(`${apiUrl}?q=${search}`);;
+		return response.json();
+	} catch (e) {
+		throw new Error('Error searching movies')
+	}
 }
